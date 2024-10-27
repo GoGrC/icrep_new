c6d01fe1791edf7669c45fd6ab9c1a8414665d54
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-10-27 19:59:18 +0530

    checked/update

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,23 +1,21 @@
-import React from "react"
+import React from "react";
 import "./Homenew.css";
-import AboutCard from "../about/AboutCard"
-import Hblog from "./Hblog"
-import HAbout from "./HAbout"
-import Hero from "./hero/Hero"
-import Hprice from "./Hprice"
-import Testimonal from "./testimonal/Testimonal"
-import HomeCard1 from "./HomeCard1"
-import HomeCard2 from "./HomeCard2"
-
-
+import AboutCard from "../about/AboutCard";
+import Hblog from "./Hblog";
+import HAbout from "./HAbout";
+import Hero from "./hero/Hero";
+import Hprice from "./Hprice";
+import Testimonal from "./testimonal/Testimonal";
+import HomeCard1 from "./HomeCard1";
+import HomeCard2 from "./HomeCard2";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <div className="homecontainer">
-        <HomeCard2/>
-        <HomeCard1/>
+        <HomeCard2 />
+        <HomeCard1 />
       </div>
       <AboutCard />
       <HAbout />
@@ -25,7 +23,7 @@ const Home = () => {
       <Hblog />
       <Hprice />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
